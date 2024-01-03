1ea4e587e6d96dd99fe39d40c3b4303784c28cb4
lampita <jose.lamparelli@gmail.com>
2024-01-02 23:56:27 -0300

    version con una sola fila

--- src/pages/components/nav-bar/cart_nav_bar.jsx
+++ src/pages/components/nav-bar/cart_nav_bar.jsx
@@ -8,25 +8,51 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Image from "react-bootstrap/Image";
 import Badge from "react-bootstrap/Badge";
+import { Col, Row } from "react-bootstrap";
 
 function CartNavBar() {
   return (
-    <Navbar collapseOnSelect expand="sm" className="bg-body-tertiary">
-      <Container style={{backgroundColor:"green" }}>
-        <Image src="nav-bar/logo.jpg" fluid style={{ width: "55px", backgroundColor:"red" }} />
-        <Navbar.Brand href="#home" style={{backgroundColor:"red" }}>Maluma-Store</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <InputGroup style={{backgroundColor:"purple" }}> 
-            <Form.Control placeholder="Buscador" aria-label="Buscador" />
-            <Button variant="outline-secondary">Button</Button>
-          </InputGroup>
-          <div style={{backgroundColor:"orange", display:"flex", flexWrap:"nowrap", alignItems:"center"}}>
-            <img src="/nav-bar/carrito.svg" alt="" />
-            <Badge bg="secondary">0</Badge>
-           
-          </div>
-        </Navbar.Collapse>
+    <Navbar collapseOnSelect expand="sm">
+      <Container fluid style={{ background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,109,147,1) 46%, rgba(10,46,54,1) 100%);" }}>
+        
+            <Image
+              src="nav-bar/logo.jpg"
+                    style={{ width: "55px", backgroundColor: "red",}}
+            />
+
+            <Navbar.Brand className="col-3" href="#home" style={{ backgroundColor: "red" }}>
+              Maluma-Store
+            </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse className="col-3" id="responsive-navbar-nav">
+            <Container style={{
+                backgroundColor: "black",
+                display: "flex",
+                flexWrap: "nowrap",
+                justifyContent: "space-between",
+                
+              }}>
+   <InputGroup  style={{ backgroundColor: "purple", width:"80%"}}>
+              <Form.Control placeholder="Buscador" aria-label="Buscador" />
+              <Button variant="outline-secondary">Buscar</Button>
+            </InputGroup>
+            <div
+
+              style={{
+                backgroundColor: "orange",
+                display: "flex",
+                flexWrap: "nowrap",
+                alignItems: "center",
+                
+              }}
+            >
+              <img src="/nav-bar/carrito.svg" alt="" />
+              <Badge bg="danger">0</Badge>
+            </div>
+            </Container>
+          </Navbar.Collapse>
+     
       </Container>
     </Navbar>
   );
