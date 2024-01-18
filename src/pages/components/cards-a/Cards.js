1821b1e2740c2bf45eab0e65824ec3cb8808d2e0
FabitoFast <fabioantunez@gmail.com>
2024-01-17 21:30:00 -0300

    src/pages/components/cards-a/Cards.js

--- src/pages/components/cards-a/Cards.js
+++ src/pages/components/cards-a/Cards.js
@@ -1,26 +1,27 @@
-import Button from '@/pages/components/cards-a/Button';
+import Button from './Button'
 
 
 
 
-const Card = () => {
+const Cards = ({tarjetasvarias}) => {
+
+    const {img, title, bgcolor} = tarjetasvarias;
+
     return (
         <>
         <figure>
-            <img src="aca va url de imagen"></img>
+            <img src={img} alt={title}/>
             <figcaption>
-                <h3>aca va algo</h3>
+
             </figcaption>
-            <p>aca va el texto de la tarjeta </p>
-            <Button/>
         </figure>
 
         <style jsx>
             {`
                 figure {
                     width: 200px;
-                    height: 275px;
-                    border: 2px solid black;
+                    height: 245px;
+                    border: 1px solid black;
                     display: flex;
                     flex-direction: column;
                     align-items: center;
@@ -30,19 +31,11 @@ const Card = () => {
                     height: 45%
                     object-fit: cover;
                 }
-                h3 {
-                    font-size: 1 rem;
-                    text-align: center;
-                    line-height: 0;
-                }
-                p{
-                    font-size: .8rem;
-                    text-align: center;
-                }
+              
             `}
         </style>
         </>
     )
 }
 
-export default Card
+export default Cards;
