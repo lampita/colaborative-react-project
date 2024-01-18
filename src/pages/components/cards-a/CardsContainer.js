96b789cf0b803a4c33d220d0458822498cbdffcd
FabitoFast <fabioantunez@gmail.com>
2024-01-17 21:55:58 -0300

    src/pages/components/cards-a/CardsContainer.js

--- src/pages/components/cards-a/CardsContainer.js
+++ src/pages/components/cards-a/CardsContainer.js
@@ -1,76 +1,67 @@
-import Cards from '@/pages/components/cards-a/Cards'
-import AIRPORT-SIMULATOR from './../../../../public/img-cards-a/AIRPORT-SIMULATOR.jpg';
-import ALIEN-6 from './../../../../public/img-cards-a/ALIEN-6.png';
-import AMONG-US from './.././../../public/img-cards-a/AMONG-US.jpg';
-import ANTHEM from './../../../../public/img-cards-a/ANTHEM.png';
-import BATTLE-FIELD from './../../../../public/img-cards-a/BATTLEFIELD.jpg';
-import AO-TENNIS from './../../../../public/img-cards-a/AO-TENNIS.jpg';
-import ASSASSING CREED from './../../../../public/img-cards-a/ASSASSING-CREED.jpg';
-import BEN-10 from './../../../../public/img-cards-a/BEN10.png';
-import BORDERLANDS from './../../../../public/img-cards-a/BORDERLANDS.jpg';
-import ASSET-CORSA from './../../../../public/img-cards-a/ASSETTO-CORSA.jpg';
+import Cards from './Cards'
+
 
 
 
 
 const tarjetas = [
     {
-    id:1,
-    img: AIRPORT-SIMULATOR,
+    id:"1",
+    img: "img-cards-a/AIRPORT-SIMULATOR.jpg",
     title: "Airport Simulator",
     bgcolor: "blue",
     },
     {
     id:2,
-    img: ALIEN-6,
+    img: "img-cards-a/ALIEN-6.png",
     title: "Alien 6",
     bgcolor: "blue",
     },
     {
     id:3,
-    img: AMONG-US,
+    img: "img-cards-a/AMONG-US.jpg",
     title: "Among Us",
     bgcolor: "blue",
     },
     {
     id:4,
-    img: ANTHEM,
+    img: "img-cards-a/ANTHEM.png",
     title: "Anthem",
     bgcolor: "blue",
     },
     {
     id:5,
-    img: BORDERLANDS,
+    img: "img-cards-a/BORDERLANDS.jpg",
     title: "Borderlands 3",
     bgcolor: "blue",
     },
     {
     id:6,
-    img: AO-TENNIS,
+    img: "img-cards-a/AO-TENNIS.jpg",
     title: "Australian Open Tennis",
     bgcolor: "blue",
     },
     {
     id:7,
-    img: ASSASSING-CREED,
+    img: "img-cards-a/ASSASSING-CREED.jpg",
     title: "Assassing Creed",
     bgcolor: "blue",
     },
     {
     id:8,
-    img: ASSETTO-CORSA,
+    img: "img-cards-a/ASSETTO-CORSA.jpg",
     title: "Assetto Corsa",
     bgcolor: "blue",
     },
     {
     id:9,
-    img: BATTLEFIELD,
+    img: "img-cards-a/BATTLEFIELD.jpg",
     title: "BattleField",
     bgcolor: "blue",
     },
     {
     id:10,
-    img: BEN-10,
+    img: "img-cards-a/BEN10.png",
     title: "Ben 10",
     bgcolor: "blue",
     }
@@ -81,7 +72,7 @@ const CardsContainer = () => {
       <>
         <div>
             {
-            tarjetas.map(tarjetasvarias => <Cards />)
+            tarjetas.map(tarjetasvarias => <Cards tarjetasvarias= {tarjetasvarias} />)
             }
         </div>
 
