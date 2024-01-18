22506c94b5f5400baabb474c4524b5d0caf3b706
FabitoFast <fabioantunez@gmail.com>
2024-01-17 21:29:57 -0300

    agregando las 10 iamgenes de cards-a

--- src/pages/components/cards-a/CardsContainer.js
+++ src/pages/components/cards-a/CardsContainer.js
@@ -1,16 +1,87 @@
 import Cards from '@/pages/components/cards-a/Cards'
+import AIRPORT-SIMULATOR from './../../../../public/img-cards-a/AIRPORT-SIMULATOR.jpg';
+import ALIEN-6 from './../../../../public/img-cards-a/ALIEN-6.png';
+import AMONG-US from './.././../../public/img-cards-a/AMONG-US.jpg';
+import ANTHEM from './../../../../public/img-cards-a/ANTHEM.png';
+import BATTLE-FIELD from './../../../../public/img-cards-a/BATTLEFIELD.jpg';
+import AO-TENNIS from './../../../../public/img-cards-a/AO-TENNIS.jpg';
+import ASSASSING CREED from './../../../../public/img-cards-a/ASSASSING-CREED.jpg';
+import BEN-10 from './../../../../public/img-cards-a/BEN10.png';
+import BORDERLANDS from './../../../../public/img-cards-a/BORDERLANDS.jpg';
+import ASSET-CORSA from './../../../../public/img-cards-a/ASSETTO-CORSA.jpg';
 
 
 
 
-const tarjetas = [1,2,3,4,5,6,7,8,9,10]
+const tarjetas = [
+    {
+    id:1,
+    img: AIRPORT-SIMULATOR,
+    title: "Airport Simulator",
+    bgcolor: "blue",
+    },
+    {
+    id:2,
+    img: ALIEN-6,
+    title: "Alien 6",
+    bgcolor: "blue",
+    },
+    {
+    id:3,
+    img: AMONG-US,
+    title: "Among Us",
+    bgcolor: "blue",
+    },
+    {
+    id:4,
+    img: ANTHEM,
+    title: "Anthem",
+    bgcolor: "blue",
+    }
+    {
+    id:5,
+    img: BORDERLANDS,
+    title: "Borderlands 3",
+    bgcolor: "blue",
+    }
+    {
+    id:6,
+    img: AO-TENNIS,
+    title: "Australian Open Tennis",
+    bgcolor: "blue",
+    }
+    {
+    id:7,
+    img: ASSASSING-CREED,
+    title: "Assassing Creed",
+    bgcolor: "blue",
+    }
+    {
+    id:8,
+    img: ASSETTO-CORSA,
+    title: "Assetto Corsa",
+    bgcolor: "blue",
+    }
+    {
+    id:9,
+    img: BATTLEFIELD,
+    title: "BattleField",
+    bgcolor: "blue",
+    }
+    {
+    id:10,
+    img: BEN-10,
+    title: "Ben 10",
+    bgcolor: "blue",
+    }
+];
 
 const CardsContainer = () => {
     return (
       <>
         <div>
             {
-            tarjetas.map(tarjetasvarias => <Card />)
+            tarjetas.map(tarjetasvarias => <Cards />)
             }
         </div>
 
